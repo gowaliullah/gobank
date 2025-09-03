47f89d46062f1fb3bf59d7e4d4340d58d66c8bb6
MD Waliullah <waliullah.godev@gmail.com>
2025-09-03 21:26:12 +0600

    newAccount func

--- typees.go
+++ typees.go
@@ -1,9 +1,20 @@
 package main
 
+import "math/rand"
+
 type Account struct {
 	ID        int
 	FirstName string
 	LastName  string
 	Number    int64
 	Balance   int64
+}
+
+func NewAccount(FirstName, LastName string) *Account {
+	return &Account{
+		ID:        rand.Intn(1000),
+		FirstName: FirstName,
+		LastName:  LastName,
+		Number:    int64(rand.Intn(1000000)),
+	}
 }
