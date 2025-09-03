5f25497addec19971ea348eebbb9c7cc41ccda26
MD Waliullah <waliullah.godev@gmail.com>
2025-09-03 20:13:17 +0600

    write a WriteJSON func

--- api.go
+++ api.go
@@ -1,6 +1,31 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func WriteJSON(w http.ResponseWriter, status int, v any) error {
+	w.WriteHeader(status)
+	w.Header().Set("Content-Type", "application/json")
+	return json.NewEncoder(w).Encode(v)
+}
+
+type apiFunc func(w http.ResponseWriter, r *http.Request) error
+
+type apiError struct {
+	Error string
+}
+
+func makeHTTPHandleFunc(f apiFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := f(w, r); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+		}
+	}
+}
 
 type APIServer struct {
 	listenAddr string
@@ -10,6 +35,11 @@ func NewAPIServer(listenAddr string) *APIServer {
 	return &APIServer{
 		listenAddr: listenAddr,
 	}
+}
+
+func (s *APIServer) Run() {
+	router := mux.NewRouter()
+
 }
 
 func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) {
