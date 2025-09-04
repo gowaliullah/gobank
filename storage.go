db2b12d3f50a9ac0acd6ef70156e798902a465d1
MD Waliullah <waliullah.godev@gmail.com>
2025-09-04 22:32:17 +0600

    config postgres for db

--- storage.go
+++ storage.go
@@ -1,8 +1,28 @@
 package main
 
+import "database/sql"
+
 type Storage interface {
 	CreateAccount(*Account) error
 	DeleteAccount(int) error
 	UpdateAccount(*Account) error
 	GetAccountByID(int) (*Account, error)
+}
+
+type PostgresStore struct {
+	db *sql.DB
+}
+
+func NewPostgresStore() (*PostgresStore, error) {
+	connStr := "user=postgres dbname=postgres password=gobank sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return &PostgresStore{db: db}, nil
 }
